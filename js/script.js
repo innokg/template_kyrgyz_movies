06d88c3dc196e34243d84ed2539355d0bbe28835
Ulan Chokushov <ulan.chokushov@gmail.com>
2022-02-01 11:44:57 +0600

    created shablon

---
+++ js/script.js
@@ -0,0 +1,153 @@
+/* Задания на урок:
+
+1) Удалить все рекламные блоки со страницы (правая часть сайта)
+
+2) Изменить жанр фильма, поменять "комедия" на "драма"
+
+3) Изменить задний фон постера с фильмом на изображение "bg.jpg". Оно лежит в папке img.
+Реализовать только при помощи JS
+
+4) Список фильмов на странице сформировать на основании данных из этого JS файла.
+Отсортировать их по алфавиту 
+
+5) Добавить нумерацию выведенных фильмов */
+
+
+
+/* Задания на урок:
+
+1) Реализовать функционал, что после заполнения формы и нажатия кнопки "Подтвердить" - 
+новый фильм добавляется в список. Страница не должна перезагружаться.
+Новый фильм должен добавляться в movieDB.movies.
+Для получения доступа к значению input - обращаемся к нему как input.value;
+P.S. Здесь есть несколько вариантов решения задачи, принимается любой, но рабочий.
+
+2) Если название фильма больше, чем 21 символ - обрезать его и добавить три точки
+
+3) При клике на мусорную корзину - элемент будет удаляться из списка (сложно)
+
+4) Если в форме стоит галочка "Сделать любимым" - в консоль вывести сообщение: 
+"Добавляем любимый фильм"
+
+5) Фильмы должны быть отсортированы по алфавиту */
+
+// Возьмите свой код из предыдущей практики
+
+
+'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const movieDB = {
+        movies: [
+            "Логан",
+            "Лига справедливости",
+            "Ла-ла лэнд",
+            "Одержимость",
+            "Скотт Пилигрим против..."
+        ]
+    };
+    
+    // const adv = document.getElementsByClassName("promo__adv"),
+    //       title = document.getElementById('title'),
+    //       bg = document.getElementById('background');
+    
+    // adv[0].remove();
+    // title.innerHTML = 'Драма';
+    // bg.style.backgroundImage = "url('img/bg.jpg')";
+    
+    
+    const adv = document.querySelectorAll('.promo__adv img'),
+          poster = document.querySelector('.promo__bg'),
+          genre = poster.querySelector('.promo__genre'),
+          movieList = document.querySelector('.promo__interactive-list'),
+          addForm = document.querySelector('form.add'),
+          addInput = addForm.querySelector('.adding__input'),
+          checkbox = addForm.querySelector('[type="checkbox"]');
+
+
+
+    addForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+
+        let newFilm = addInput.value;
+        const favorite = checkbox.checked;
+
+
+        if (newFilm) {
+
+            if (newFilm.length > 21) {
+                newFilm = `${newFilm.substring(0, 22)}...`;
+            }
+
+            movieDB.movies.push(newFilm);
+            sortArr(movieDB.movies);
+    
+            createMovielist(movieDB.movies, movieList);
+        }
+      
+        event.target.reset();
+
+
+    });
+    
+    const deleteAdv = (arr) => {
+        arr.forEach(item => {
+            item.remove();
+        });
+        
+    };
+
+   
+ 
+   
+    const makeChanges = () => {
+        genre.textContent = "Драма";
+    
+        poster.style.backgroundImage = "url('img/bg.jpg')";
+    };
+
+  
+
+    const sortArr = (arr) => {
+        arr.sort();
+    };
+
+
+   
+
+    function createMovielist(films, parent) {
+        parent.innerHTML = "";
+                   
+        films.forEach((film, i) => {
+            parent.innerHTML += `
+            <li class="promo__interactive-item"> ${i + 1} ${film}
+                <div class="delete"></div>
+            </li>
+            `;
+        });
+
+
+        document.querySelectorAll('.delete').forEach((btn, i) => {
+            btn.addEventListener('click', () => {
+                btn.parentElement.remove();
+                movieDB.movies.splice(i, 1);
+                
+
+                createMovielist(films, parent);
+            });
+        });
+    }
+
+    deleteAdv(adv);
+    makeChanges();
+    sortArr(movieDB.movies);
+    createMovielist(movieDB.movies, movieList);
+
+});
+
+
+
+
+
+
+
